edef9d6c5eeb6fb5563d66eb38186fcfcdddd7b8
HP <HP@192.168.1.3>
2026-01-28 19:16:43 +0530

    svdhgfuw

---
+++ Demo/src/com/demo/IdentifierDemo.java
@@ -0,0 +1,26 @@
+package com.demo;
+
+public class IdentifierDemo {
+
+	public static void main(String[] args) {
+		
+		
+		//varible declaration 
+		//int static=13;  // error bcoz keyword cannot be identifier 
+		
+		//int num1=13;  error bcoz do  ot add space 
+		
+		//int num=13; error boca speacial symbols not allowed 
+		
+		int $num1 = 20;
+		System.out.println("Value of the number is :"+ $num1);
+		
+		String studentName="Aditya";
+		System.out.println("Student name is :"+ studentName);
+		
+		char Gender='M';
+		System.out.println("Gender :"+Gender);
+
+	}
+
+}
